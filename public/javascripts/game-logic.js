89f2807285512950c84420364abb8c79b068baf0
Utsab <utsab.k.saha@gmail.com>
2017-10-09 01:53:56 +0000

    Detect winner

--- public/javascripts/game-logic.js
+++ public/javascripts/game-logic.js
@@ -1,27 +1,70 @@
+let isXTurn = true; 
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-    let isXTurn = true; 
-    
+function assignClickHandlers() {
     var squares = document.getElementsByClassName('square');
-
-    // get the number of selected elements
-    console.log("num squares: " + squares.length);
-    
-    // iterate over elements and add a click handler
-    
-    function handleSquareClick(e) {
-        
-        if (e.target.innerHTML) 
-            return; 
-            
-        alert("clicked on square: " + e.target.dataset.position); 
-        e.target.innerHTML = isXTurn ? "X" : "O";
-        isXTurn = !isXTurn; 
-    }
-    
- 
     for (var i = 0; i < squares.length; i++) {
         squares[i].addEventListener('click', handleSquareClick);
     }
+}
+    
+function calculateWinner() {
+    
+     const lines = [
+        [0, 1, 2], 
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ]; 
+      
+     
+     var squares = document.getElementsByClassName('square');
+     
+     for (var i = 0; i < lines.length; i++) {
+        var a = lines[i][0]; 
+        var b = lines[i][1];
+        var c = lines[i][2]; 
+        
+        
+        if (squares[a].innerHTML && squares[a].innerHTML === squares[b].innerHTML && squares[a].innerHTML === squares[c].innerHTML) {
+          return squares[a].innerHTML; 
+        }
+     }
+     
+     return null; 
+          
+}
+
+
+function handleSquareClick(e) {
+    if (e.target.innerHTML || calculateWinner()) 
+        return; 
+        
+    // alert("clicked on square: " + e.target.dataset.position); 
+    e.target.innerHTML = isXTurn ? "X" : "O";
+    isXTurn = !isXTurn; 
+    showGameStatus(); 
+}
+    
+function showGameStatus() {
+    var winner = calculateWinner(); 
+    var statusView = document.getElementsByClassName('status')[0]; 
+    
+    if (winner) {
+        statusView.innerHTML = "Winner: " + winner; 
+    } else {
+        statusView.innerHTML = "Next player: " + (isXTurn ? "X" : "O"); 
+    }
+        
+}
+    
+    
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    assignClickHandlers(); 
+    showGameStatus(); 
 });
+
