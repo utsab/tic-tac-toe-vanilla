f65040b6d982cff20b4d32127b1ed6315529838c
Utsab <utsab.k.saha@gmail.com>
2017-10-09 03:18:33 +0000

    Refactor code to separate data from view

--- public/javascripts/game-logic.js
+++ public/javascripts/game-logic.js
@@ -1,4 +1,6 @@
 let isXTurn = true; 
+var squareValues = Array(9).fill(null); 
+
 
 function assignClickHandlers() {
     var squares = document.getElementsByClassName('square');
@@ -20,17 +22,14 @@ function calculateWinner() {
         [2, 4, 6],
       ]; 
       
-     
-     var squares = document.getElementsByClassName('square');
-     
      for (var i = 0; i < lines.length; i++) {
         var a = lines[i][0]; 
         var b = lines[i][1];
         var c = lines[i][2]; 
         
         
-        if (squares[a].innerHTML && squares[a].innerHTML === squares[b].innerHTML && squares[a].innerHTML === squares[c].innerHTML) {
-          return squares[a].innerHTML; 
+        if (squareValues[a] && squareValues[a] === squareValues[b] && squareValues[a] === squareValues[c]) {
+          return squareValues[a]; 
         }
      }
      
@@ -44,27 +43,76 @@ function handleSquareClick(e) {
         return; 
         
     // alert("clicked on square: " + e.target.dataset.position); 
-    e.target.innerHTML = isXTurn ? "X" : "O";
+    squareValues[e.target.dataset.position] = isXTurn ? "X" : "O"; 
     isXTurn = !isXTurn; 
-    showGameStatus(); 
+    renderBoardView(); 
 }
     
-function showGameStatus() {
+function getGameStatus() {
     var winner = calculateWinner(); 
-    var statusView = document.getElementsByClassName('status')[0]; 
     
     if (winner) {
-        statusView.innerHTML = "Winner: " + winner; 
+        return "Winner: " + winner; 
     } else {
-        statusView.innerHTML = "Next player: " + (isXTurn ? "X" : "O"); 
+        return  "Next player: " + (isXTurn ? "X" : "O"); 
     }
         
 }
     
+
+function destroyBoardView() {
+    var rootDiv = document.getElementById("root");
+    rootDiv.removeChild(rootDiv.firstChild); 
+}
+
+function renderBoardView() {
+    destroyBoardView(); 
     
+    // All of our generated HTML will be appended to the root div 
+    var rootDiv = document.getElementById("root");
+    
+    // create the game div and append to root
+    var gameDiv = document.createElement('div');
+    gameDiv.className = 'game';
+    rootDiv.appendChild(gameDiv); 
+    
+    
+    // create the game-board and append to game
+    var gameBoardDiv = document.createElement('div');
+    gameBoardDiv.className = 'game-board';
+    gameDiv.appendChild(gameBoardDiv); 
+    
+    // add game status to the game-board
+    var gameStatusDiv = document.createElement('div');
+    gameStatusDiv.className = 'status';
+    gameStatusDiv.innerHTML = getGameStatus(); 
+    gameBoardDiv.appendChild(gameStatusDiv); 
+    
+    // add 3 rows with 3 buttons each to make the board
+    
+    for (var i = 0; i < 3; i++) {
+        var boardRowDiv = document.createElement('div');
+        boardRowDiv.className = 'board-row';
+        gameBoardDiv.appendChild(boardRowDiv);
+        
+        for (var j = 0; j < 3; j++) {
+            var squareButton = document.createElement('button');
+            squareButton.className = 'square';
+            squareButton.dataset.position = i*3 + j; 
+            squareButton.innerHTML = squareValues[squareButton.dataset.position]; 
+            boardRowDiv.appendChild(squareButton);
+        }
+        
+        gameBoardDiv.appendChild(boardRowDiv); 
+    }
+    
+    assignClickHandlers(); 
+    //document.getElementById('item1').dataset.icon = "base.gif";
+}
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-    assignClickHandlers(); 
-    showGameStatus(); 
+    // assignClickHandlers(); 
+    // showGameStatus(); 
+    renderBoardView(); 
 });
 
