423ba348bee206cab8d8202c71a435caddfd3bbe
Utsab <utsab.k.saha@gmail.com>
2017-10-09 04:35:45 +0000

    Add a separate render methods for Board, Game, and Square.  Store the squares in a central 'model' object that is passed from Game to Board to Square

--- public/javascripts/game-logic.js
+++ public/javascripts/game-logic.js
@@ -1,118 +1,247 @@
-let isXTurn = true; 
-var squareValues = Array(9).fill(null); 
 
 
-function assignClickHandlers() {
-    var squares = document.getElementsByClassName('square');
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].addEventListener('click', handleSquareClick);
+class Model {
+    constructor() {
+        this._isXTurn = true; 
+        this._squareValues = Array(9).fill(null); 
+    }
+    
+    toggleTurn() {
+        this._isXTurn = !this._isXTurn; 
+    }
+    
+    getIsXTurn() {
+        return this._isXTurn; 
+    }
+    
+    setMove(index, val) {
+        this._squareValues[index] = val; 
+    }
+    
+    getSquareValues() {
+        return this._squareValues; 
     }
 }
+
+ 
+
+/*
+Plan: 
+*  Have a Game class
+* Board class 
+
+*/
+class Board {
+
+    constructor(model) {
+        this.model = model; 
+    }
     
-function calculateWinner() {
+    render() {
+        // add game status to the game-board
+        var containerDiv = document.createElement('div'); 
+        
+        var gameStatusDiv = document.createElement('div');
+        gameStatusDiv.className = 'status';
+        //gameStatusDiv.innerHTML = getGameStatus(); //TODO: uncomment this
+        containerDiv.appendChild(gameStatusDiv); 
+        
+        // add 3 rows with 3 buttons each to make the board
+        
+        for (var i = 0; i < 3; i++) {
+            var boardRowDiv = document.createElement('div');
+            boardRowDiv.className = 'board-row';
+            containerDiv.appendChild(boardRowDiv);
+            
+            for (var j = 0; j < 3; j++) {
+                var squareButton = new Square(this.model, j*3 + i); 
+                boardRowDiv.appendChild(squareButton.render());
+            }
+        }
+        
+        return containerDiv; 
+    }
+}
+
+class Game {
     
-     const lines = [
-        [0, 1, 2], 
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ]; 
+    constructor() {
+        this.model = new Model();
+        this.board = new Board(this.model); 
+    }
+    
+    destroyView() {
+        var rootDiv = document.getElementById("root");
+        rootDiv.removeChild(rootDiv.firstChild); 
+    }
+    
+    render() {
+        var gameDiv = document.createElement('div');
+        gameDiv.className = 'game';
+        gameDiv.appendChild(this.board.render()); 
+        return gameDiv; 
+    } 
+    
+}
+
+class Square {
+    constructor(model, position) {
+        this.model = model; 
+        this.position = position; 
+    }
+    
+    handleSquareClick(e) {
+        // if (e.target.innerHTML || calculateWinner()) 
+        //     return; 
+            
+        alert("clicked on square: " + e.target.dataset.position); 
+        var squareValues = this.model.getSquareValues(); 
+        var isXTurn = this.model.getIsXTurn(); 
+        
+        squareValues[e.target.dataset.position] = isXTurn ? "X" : "O"; 
+        console.log("in handleSquareSlick: setting position " + e.target.dataset.position + " to " + squareValues[e.target.dataset.position]); 
+        console.log(squareValues); 
+        
+        //isXTurn = !isXTurn; 
+        //renderBoardView(); 
+    }
+    
+    assignClickHandler(squareButton) {
+        squareButton.addEventListener('click', this.handleSquareClick.bind(this));
+    
+    }
+
+    
+    render() {
+        var squareButton = document.createElement('button');
+        squareButton.className = 'square';
+        squareButton.dataset.position = this.position; 
+        squareButton.innerHTML = this.model.getSquareValues()[squareButton.dataset.position]; 
+        this.assignClickHandler(squareButton);  
+        return squareButton; 
+    }
+}
+
+
+// function assignClickHandlers() {
+//     var squares = document.getElementsByClassName('square');
+//     console.log("num squares: " + squares.length); 
+    
+//     for (var i = 0; i < squares.length; i++) {
+//         squares[i].addEventListener('click', handleSquareClick);
+//     }
+// }
+    
+    
+// function calculateWinner() {
+    
+//      const lines = [
+//         [0, 1, 2], 
+//         [3, 4, 5],
+//         [6, 7, 8],
+//         [0, 3, 6],
+//         [1, 4, 7],
+//         [2, 5, 8],
+//         [0, 4, 8],
+//         [2, 4, 6],
+//       ]; 
       
-     for (var i = 0; i < lines.length; i++) {
-        var a = lines[i][0]; 
-        var b = lines[i][1];
-        var c = lines[i][2]; 
+//      for (var i = 0; i < lines.length; i++) {
+//         var a = lines[i][0]; 
+//         var b = lines[i][1];
+//         var c = lines[i][2]; 
         
         
-        if (squareValues[a] && squareValues[a] === squareValues[b] && squareValues[a] === squareValues[c]) {
-          return squareValues[a]; 
-        }
-     }
+//         if (squareValues[a] && squareValues[a] === squareValues[b] && squareValues[a] === squareValues[c]) {
+//           return squareValues[a]; 
+//         }
+//      }
      
-     return null; 
+//      return null; 
           
-}
+// }
 
 
-function handleSquareClick(e) {
-    if (e.target.innerHTML || calculateWinner()) 
-        return; 
+// function handleSquareClick(e) {
+//     // if (e.target.innerHTML || calculateWinner()) 
+//     //     return; 
         
-    // alert("clicked on square: " + e.target.dataset.position); 
-    squareValues[e.target.dataset.position] = isXTurn ? "X" : "O"; 
-    isXTurn = !isXTurn; 
-    renderBoardView(); 
-}
+//     alert("clicked on square: " + e.target.dataset.position); 
+//     squareValues[e.target.dataset.position] = isXTurn ? "X" : "O"; 
+//     isXTurn = !isXTurn; 
+//     //renderBoardView(); 
+// }
     
-function getGameStatus() {
-    var winner = calculateWinner(); 
+// function getGameStatus() {
     
-    if (winner) {
-        return "Winner: " + winner; 
-    } else {
-        return  "Next player: " + (isXTurn ? "X" : "O"); 
-    }
+//     var winner = calculateWinner(); 
+    
+//     if (winner) {
+//         return "Winner: " + winner; 
+//     } else {
+//         return  "Next player: " + (isXTurn ? "X" : "O"); 
+//     }
         
-}
+// }
     
 
-function destroyBoardView() {
-    var rootDiv = document.getElementById("root");
-    rootDiv.removeChild(rootDiv.firstChild); 
-}
+// function destroyBoardView() {
+//     var rootDiv = document.getElementById("root");
+//     rootDiv.removeChild(rootDiv.firstChild); 
+// }
 
-function renderBoardView() {
-    destroyBoardView(); 
+// function renderBoardView() {
+//     destroyBoardView(); 
     
-    // All of our generated HTML will be appended to the root div 
-    var rootDiv = document.getElementById("root");
+//     // All of our generated HTML will be appended to the root div 
+//     var rootDiv = document.getElementById("root");
     
-    // create the game div and append to root
-    var gameDiv = document.createElement('div');
-    gameDiv.className = 'game';
-    rootDiv.appendChild(gameDiv); 
+//     // create the game div and append to root
+//     var gameDiv = document.createElement('div');
+//     gameDiv.className = 'game';
+//     rootDiv.appendChild(gameDiv); 
     
     
-    // create the game-board and append to game
-    var gameBoardDiv = document.createElement('div');
-    gameBoardDiv.className = 'game-board';
-    gameDiv.appendChild(gameBoardDiv); 
+//     // create the game-board and append to game
+//     var gameBoardDiv = document.createElement('div');
+//     gameBoardDiv.className = 'game-board';
+//     gameDiv.appendChild(gameBoardDiv); 
     
-    // add game status to the game-board
-    var gameStatusDiv = document.createElement('div');
-    gameStatusDiv.className = 'status';
-    gameStatusDiv.innerHTML = getGameStatus(); 
-    gameBoardDiv.appendChild(gameStatusDiv); 
+//     // add game status to the game-board
+//     var gameStatusDiv = document.createElement('div');
+//     gameStatusDiv.className = 'status';
+//     gameStatusDiv.innerHTML = getGameStatus(); 
+//     gameBoardDiv.appendChild(gameStatusDiv); 
     
-    // add 3 rows with 3 buttons each to make the board
+//     // add 3 rows with 3 buttons each to make the board
     
-    for (var i = 0; i < 3; i++) {
-        var boardRowDiv = document.createElement('div');
-        boardRowDiv.className = 'board-row';
-        gameBoardDiv.appendChild(boardRowDiv);
+//     for (var i = 0; i < 3; i++) {
+//         var boardRowDiv = document.createElement('div');
+//         boardRowDiv.className = 'board-row';
+//         gameBoardDiv.appendChild(boardRowDiv);
         
-        for (var j = 0; j < 3; j++) {
-            var squareButton = document.createElement('button');
-            squareButton.className = 'square';
-            squareButton.dataset.position = i*3 + j; 
-            squareButton.innerHTML = squareValues[squareButton.dataset.position]; 
-            boardRowDiv.appendChild(squareButton);
-        }
+//         for (var j = 0; j < 3; j++) {
+//             var squareButton = document.createElement('button');
+//             squareButton.className = 'square';
+//             squareButton.dataset.position = i*3 + j; 
+//             squareButton.innerHTML = squareValues[squareButton.dataset.position]; 
+//             boardRowDiv.appendChild(squareButton);
+//         }
         
-        gameBoardDiv.appendChild(boardRowDiv); 
-    }
+//         gameBoardDiv.appendChild(boardRowDiv); 
+//     }
     
-    assignClickHandlers(); 
-    //document.getElementById('item1').dataset.icon = "base.gif";
-}
+//     assignClickHandlers(); 
+//     //document.getElementById('item1').dataset.icon = "base.gif";
+// }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     // assignClickHandlers(); 
     // showGameStatus(); 
-    renderBoardView(); 
+    //renderBoardView(); 
+    
+    var game = new Game(); 
+    var rootDiv = document.getElementById("root");
+    rootDiv.appendChild(game.render()); 
 });
 
